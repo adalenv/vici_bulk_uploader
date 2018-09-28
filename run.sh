2ec769a37737775b7fc42b8fe2d110520cd94fca
adalenv <adalenvladi@gmail.com>
2018-09-28 17:35:57 +0200

    updates

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
- for filename in ./*.txt; do 
-	php up_cli  --file_name=$filename  --campaign_id=1000;
+for filename in ./*.txt; do 
+ 	[ -f "$filename" ] || echo 'No file left!'; #break
+	php up_cli  --file_name=$filename  --campaign_id=$1; #1000
 done;
